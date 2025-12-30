c2e6feec22634b3847b824d779a22cac6b55ffa9
aestasxfavor <pstar0819@naver.com>
2025-12-30 15:26:39 +0900

    오목게임 새로 만들기 및 내일 새로운 거 만들예정

--- InventoryEx/InventoryEx/obj/Debug/net8.0/InventoryEx.AssemblyInfo.cs
+++ InventoryEx/InventoryEx/obj/Debug/net8.0/InventoryEx.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InventoryEx")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d004b5ee7b624c300566eadd89a336b2a70ddefd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4224496fc0278e7404999f3f836e20e25de913aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("InventoryEx")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InventoryEx")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ InventoryEx/OmokEx/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OmokEx
+{
+    public class Omok
+    {
+        private int size;
+        private int[,] seat;
+
+        private int curX;
+        private int curY;
+
+        public int CurX
+        {
+            get { return curX; }
+            set { curX = value; }
+        }
+
+        private bool isBlackTurn;
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
