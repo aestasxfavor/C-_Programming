579ac982e4567e9925701a010b8562fffe9859e8
aestasxfavor <pstar0819@naver.com>
2026-04-05 22:52:50 +0900

    (2026-04-05) 코딩테스트

---
+++ CodingTest/CodingPractice/CodingPractice/CodingPractice.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+#pragma region Coiding Test 2026_04_05
+	/*
+	백준 18108번: 1998년생인 내가 태국에서는 2541년생?!
+
+	int a;
+	int b;
+
+	cin >> a;
+	b = a - 543;
+
+	cout << b << endl;
+
+	*/
+
+	/*
+	백준 10430번: 나머지
+
+	int a;
+	int b;
+	int c;
+	
+	cin >> a;
+	cin >> b;
+	cin >> c;
+	
+	cout << (a + b) % c << endl;
+	cout << ((a % c) + (b % c)) % c << endl;
+	cout << (a * b) % c << endl;
+	cout << ((a % c) * (b % c)) % c << endl;
+
+*/
+
+#pragma endregion
+
+
+}
